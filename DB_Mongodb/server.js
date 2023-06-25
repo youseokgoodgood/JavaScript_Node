b3ed9ca4770a7fee622915870cb808a9a8150b98
유석이노트북\wnsgh <wnsghrnt2586@gmail.com>
2023-06-25 23:17:43 +0900

    nodejs_mongodb 연결

--- DB_Mongodb/server.js
+++ DB_Mongodb/server.js
@@ -4,8 +4,16 @@ const postsRouters = require("./routes/posts.router");
 const PORT = 4000;
 const app = express();
 const path = require("path");
+const { default: mongoose } = require("mongoose");
 app.use(express.json());
 app.use("/static", express.static(path.join(__dirname, "public")));
+
+mongoose
+  .connect(
+    "mongodb+srv://wnsghrnt2586:MMqxz0x1eRkYwwls@express-cluster.zasrnei.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB connect Success"))
+  .catch((err) => console.error(err));
 
 app.use((req, res, next) => {
   const start = Date.now();
